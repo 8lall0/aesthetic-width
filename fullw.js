f4b8ef5e5d374d5dc63957ffcc950e3fed12a8c1
sliguori <savino.liguori@ediscom.it>
2018-02-28 14:39:00 +0100

    Version 2.1. Copy support for Firefox. Code refactoring.

--- fullw.js
+++ fullw.js
@@ -1,54 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
-	var btnArray = ['~', '©', '®'];
-
-	var field = document.getElementById('intext');
-	var message = document.getElementById('copytext');
-	var btns = document.getElementById('specButtons');
-
-    var sub = document.getElementById('fullwsub');
-    var til = document.getElementById('til');
-    var cop = document.getElementById('cop');
-    var roy = document.getElementById('roy');
-    
-    sub.addEventListener('click', function() {
-        convertAndCopy(field);
-        fadeOutIn(message, 1000);
-    });
-	addBtns(btns, btnArray, field);
+	const field = document.getElementById('intext'),
+		txtBtn = document.querySelectorAll('.textBtn');
+	for (var i = 0; i < txtBtn.length; i++) {
+		txtBtn[i].addEventListener('click', function() {
+			field.value += this.firstChild.data;
+		});
+	}
+	document.getElementById('fullwsub').addEventListener('click', convertAndCopy, false);
 });
 
-function addBtns(elem, array, intext) {
-	for (var i = 0; i < array.length; i++) { (
-		function (i) {
-			var btn = document.createElement("button");
-			var t = document.createTextNode(array[i]);
-			btn.appendChild(t);
-			elem.appendChild(btn);
-			btn.addEventListener('click', function() {
-				intext.value += array[i];
-			});
-		}) (i);
-	}
-}
-
-function convertAndCopy(elem) {
+function convertAndCopy() {
 	const fwidth = 0xFEE0;
-	elem.value = elem.value.replace(/[\u0000-\u007F]/g, function(ch) {
+	const fld = document.getElementById('intext'),
+		message = document.getElementById('copytext');
+	fld.value = fld.value.replace(/[\u0000-\u007F]/g, function(ch) {
 		if (ch != " ") {
-			return String.fromCharCode(ch.charCodeAt(0) + fwidth);	
+			return String.fromCharCode(ch.charCodeAt(0) + fwidth);
 		} else {
 			return " ";
 		}
-		
-	});
 
-	elem.focus();
-	elem.select();
-	document.execCommand("Copy", true);
-	elem.blur();
+	});
+	fld.select();
+	document.execCommand("Copy");
+	fadeOutIn(message, 1000);
 }
 
-function fadeOutIn(elem, speed ) {
+function fadeOutIn(elem, speed) {
 	if (!elem.style.opacity) {
 		elem.style.opacity = 1;
 	}
@@ -57,5 +35,5 @@ function fadeOutIn(elem, speed ) {
 		if (elem.style.opacity <= 0) {
 			clearInterval(outInterval);
 		}
-	}, speed/50 );
+	}, speed / 50);
 }
